3707b39f5e5f0d0dbfa121af8e08635eef3a1d18
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2025-05-26 15:50:41 -0300

    refactor: update styles and clean up Welcome component; remove unused svgs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,15 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from '../welcome/welcome'
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'Chatbot AI Embedded' },
+    {
+      name: 'description',
+      content: 'Welcome to Chatbot AI Embedded web application!',
+    },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Welcome />
 }
